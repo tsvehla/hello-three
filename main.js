f008b3b5dc8161d5b29622321340ce8f52342b56
psvehla <psvehla@hotmail.com>
2023-08-28 00:51:19 +1000

    Made cube bigger

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(3, 3, 3);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
